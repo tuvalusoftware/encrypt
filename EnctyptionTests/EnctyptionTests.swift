0d1b59960a46f1b28a599af8afbfbda8e095f912
Hong Hoa <nguyenhoangduc061195@gmail.com>
2016-11-04 10:54:07 +0700

    Initial Commit

---
+++ EnctyptionTests/EnctyptionTests.swift
@@ -0,0 +1,36 @@
+//
+//  EnctyptionTests.swift
+//  EnctyptionTests
+//
+//  Created by Hong Hoa on 11/4/16.
+//  Copyright © 2016 nhd. All rights reserved.
+//
+
+import XCTest
+@testable import Enctyption
+
+class EnctyptionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
